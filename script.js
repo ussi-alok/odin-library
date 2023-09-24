f589ebab65af7efc4a1381df3836365130017009
ussi-alok <81419246+ussi-alok@users.noreply.github.com>
2023-09-24 18:22:18 +0530

    Object constructor created

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title,author,pages,read) {
   // the constructor...
+  this.title =title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        const concatInfo = title+" "+author+" "+pages+" "+read;
+        return concatInfo;
+    }
 }
 
 function addBookToLibrary() {
